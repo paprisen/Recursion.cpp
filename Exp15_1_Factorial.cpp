1465e34304fc51d146d1264ea565334e01860b21
Papri <papri.sen2020@gmail.com>
2025-09-28 11:32:45 +0530

    Update Exp15_1_Factorial.cpp

--- Exp15_1_Factorial.cpp
+++ Exp15_1_Factorial.cpp
@@ -2,20 +2,33 @@
 // 24070123070
 // A3
 
-#include <iostream>
+#include<iostream>
 using namespace std;
-int factorial(int n){
-    if(n==0){
+
+int fact(int n){
+    if(n<0|| n==0){
         return 1;
     }
+
     else{
-        return n*factorial(n-1);
+        return n*fact(n-1);
     }
 }
-int main() {
-    int n;
-    cout << "Enter Number to find factorial: ";
-    cin >> n;
-    cout << "Factorial is " << factorial(n);
+int main(){
+
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout<<"Factorial of the entered number is:"<<fact(num)<<endl;
+
     return 0;
 }
+
+/* 
+Outputs:
+Enter a number: 5
+Factorial of the entered number is:120
+*/
+
